6886b63836233df40ea1fd363b9ffb5f3b849e63
github username <github email address>
2021-05-17 09:42:51 -0400

    commit tree efa9f8099126b056442e9128dc368a53a8ac5ab7

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+# def square_array(array)
+#   squared = []
+#   array.each { |element| squared << element ** 2 }
+#   squared
+# end
+
+
 def square_array(array)
+  square = []
+  array.each do |number|
+    square << number ** 2
+    square
   # your code here
 end
